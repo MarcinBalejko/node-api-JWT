696250ec96cb6fa488e7f0acb2e2c339d894b007
MarcinBalejko <mbbm9@o2.pl>
2020-02-23 01:22:46 +0100

    :construction: Implementing Joi for validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,38 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+// Validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+  name: Joi.string()
+    .min(6)
+    .required(),
+  email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+  password: Joi.string()
+    .min(6)
+    .required()
+});
+
 router.post('/register', async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
-  try {
-    const savedUser = await user.save();
-    res.send(savedUser);
-  } catch (err) {
-    res.status(400).send(err);
-  }
+  // Lets validate the data before we make a user
+  const validation = schema.validate(req.body);
+  res.send(validation);
+
+  // const user = new User({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password
+  // });
+  // try {
+  //   const savedUser = await user.save();
+  //   res.send(savedUser);
+  // } catch (err) {
+  //   res.status(400).send(err);
+  // }
 });
 
 module.exports = router;
