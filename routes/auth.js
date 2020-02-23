a66fc0a341fef7c00f1f524b6bbc66caf7772c25
MarcinBalejko <mbbm9@o2.pl>
2020-02-23 21:13:46 +0100

    :white_check_mark: Implemented bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const { registerValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
+  // Validating the data before adding a new user
   const { error } = registerValidation(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
+  // Checking if the user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already exists');
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Creating a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   });
   try {
     const savedUser = await user.save();
