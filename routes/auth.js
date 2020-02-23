a9ab13292d1f6e7b21ece2ce0e10dd67b8b72f72
MarcinBalejko <mbbm9@o2.pl>
2020-02-23 01:37:29 +0100

    :white_check_mark: Users get validated on registration

--- routes/auth.js
+++ routes/auth.js
@@ -19,20 +19,22 @@ const schema = Joi.object({
 
 router.post('/register', async (req, res) => {
   // Lets validate the data before we make a user
-  const validation = schema.validate(req.body);
-  res.send(validation);
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
-  // const user = new User({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password
-  // });
-  // try {
-  //   const savedUser = await user.save();
-  //   res.send(savedUser);
-  // } catch (err) {
-  //   res.status(400).send(err);
-  // }
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
